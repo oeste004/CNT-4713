f306a74f96aa6ac6ece1db289e9d91a23a26f9b8
Olivia Estevez <oeste004@fiu.edu>
2021-09-12 19:04:29 -0400

    connection works

--- client.py
+++ client.py
@@ -1,20 +1,19 @@
 import socket
 import sys
 
-command = sys.argv
+class client:
 
-host = command[1]
-port = command[2]
-#fileName = command[3]
+    command = sys.argv
+    host = command[1]
+    port = int(command[2])
+    #fileName = command[3]
 
-success = False
 
-try:
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
-    s.connect((host, port))
-    success = True
-except:
-    success = False
 
-print(success)
+    try:
+        s.connect((host, port))
+        print("Connection succeeded.")
+    except:
+        print("Connection refused.")
