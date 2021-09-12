9386582c9d23e44adb159b6a1c8ef514d753beaa
Olivia Estevez <oeste004@fiu.edu>
2021-09-12 18:40:05 -0400

    added from assignment2

--- client.py
+++ client.py
@@ -1,1 +1,20 @@
 import socket
+import sys
+
+command = sys.argv
+
+host = command[1]
+port = command[2]
+#fileName = command[3]
+
+success = False
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+    s.connect((host, port))
+    success = True
+except:
+    success = False
+
+print(success)
