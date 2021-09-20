64b3c6b073052bc64a15af3f03be7c8f225bf373
Olivia Estevez <oeste004@fiu.edu>
2021-09-19 20:29:59 -0400

    75%

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ class client:
 
     # if port number is outside the valid range for TCP
     if port > 65535 or port < 1:
-        sys.stderr.write("ERROR: Invalid port number.")
+        sys.stderr.write("ERROR: ")
         sys.exit(1)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,10 +19,10 @@ class client:
     # connect to host and exits if not successful
     try:
         s.connect((host, port))
-        s.settimeout(4)
+        s.settimeout(5)
         print("Connection succeeded.")
     except:
-        sys.stderr.write("ERROR: Cannot connect.")
+        sys.stderr.write("ERROR: ")
         sys.exit(1)
 
     # receive data in chunks and shows error if not received properly
